5b33a2ad42dc1832c0c54b24f22a310a80c8ad45
AnthonyYao <39170787+AnthonyYao@users.noreply.github.com>
2018-10-27 18:16:24 -0400

    added enemies

--- hackGame/src/hackGame/enemy.java
+++ hackGame/src/hackGame/enemy.java
@@ -3,9 +3,6 @@ package hackGame;
 public abstract class enemy implements actors {
 	private int health,damage, sightRange,attackRange;
 	private image picture;
-	health = difficulty * playerLvl * 100;
-	damage = difficulty * playerLvl * 10;
-	sightRange = difficulty + 10;
-	attackRage = difficulty + 1;
+	
 	void generateCorpse(String monster, int lvl);
 }
